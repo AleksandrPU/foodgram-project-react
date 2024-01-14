36b71042c1c3394f49a4eaf3babc95c17d7a1e9c
Паутов Александр <sh2d000w@gmail.com>
2024-01-14 12:47:17 +0300

    fix: Delete .gitkeep feat: Add choice of CSV to import in Ingredients

--- backend/recipes/management/commands/import_ingredients_from_csv.py
+++ backend/recipes/management/commands/import_ingredients_from_csv.py
@@ -1,26 +1,35 @@
 import csv
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from recipes.models import Ingredient
 
 
-def import_ingredients():
+def import_ingredients(file):
     try:
-        with open('../../data/ingredients.csv') as csvfile:
+        with open(file) as csvfile:
             reader = csv.reader(csvfile)
             for name, unit in reader:
                 Ingredient.objects.get_or_create(
                     name=name, measurement_unit=unit)
     except FileNotFoundError:
-        raise ValueError('ingredients.csv не найден!')
+        raise CommandError(f'Файл {file} не найден!')
     except Exception as error:
-        print(error)
+        raise CommandError(error)
 
 
 class Command(BaseCommand):
-    help = 'Импорт ингредиентов из CSV'
+    help = 'Импорт ингредиентов из CSV.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--csv',
+            help='Укажите CSV для загрузки.'
+        )
 
     def handle(self, *args, **options):
-        import_ingredients()
-        self.stderr.write(self.style.SUCCESS('Данные загружены'))
+        if file := options['csv']:
+            import_ingredients(file)
+            self.stdout.write(self.style.SUCCESS('Данные загружены'))
+        else:
+            self.stderr.write(self.style.ERROR('Не указан CSV для загрузки!'))
