0cbe477bbbd3f97b63882ecb0760d13a094671fc
Паутов Александр <sh2d000w@gmail.com>
2024-01-21 14:54:11 +0300

    fix: Move subscriptions app to users app

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import Subscription, User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'role')
     list_filter = ('username', 'email')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    pass
--- backend/users/models.py
+++ backend/users/models.py
@@ -33,3 +33,37 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username[:settings.STRING_LENGTH_LIMIT]
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        related_name='followers'
+    )
+    following = models.ForeignKey(
+        User,
+        verbose_name='Подписка',
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'following'),
+                name='unique_user_following'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('following')),
+                name='no_self_follows'
+            )
+        ]
+
+    def __str__(self):
+        return (f'{self.user.username[:settings.STRING_LENGTH_LIMIT]} '
+                'подписан на '
+                f'{self.following.username[:settings.STRING_LENGTH_LIMIT]}')
