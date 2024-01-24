3e3d07c32dab5c8b61190dfe87df1d9b7d7ca220
Паутов Александр <sh2d000w@gmail.com>
2024-01-24 20:41:08 +0300

    fix: PEP8 in recipes/urls.py.

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from recipes.views import RecipeViewSet, TagViewSet, IngredientViewSet
+from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 
 router = DefaultRouter()
 
-router.register('tags', TagViewSet, basename='tags')
-router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path('', include(router.urls)),
