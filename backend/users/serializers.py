a11318125cb84dd89920ea128f004446e8a6412d
Паутов Александр <sh2d000w@gmail.com>
2024-01-24 21:50:15 +0300

    feat: Add get_is_subscribed to UserReadSerializer. fix: PEP8 in users/serializers.py.

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer
+# todo
+# from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 
 
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserReadSerializer(serializers.ModelSerializer):
     is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
@@ -23,11 +24,43 @@ class UserSerializer(serializers.ModelSerializer):
     def get_is_subscribed(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            return user.followers.filter(following__pk=obj.id).exists()
+            return obj.following.filter(user=user).exists()
         return False
 
 
-class UserRegistrationSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+# todo
+class UserAfterCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+        )
+
+
+# todo
+# class UserRegistrationSerializer(UserCreateSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
+    # class Meta(UserCreateSerializer.Meta):
+    class Meta:
+        model = User
+        # todo
         # fields = ('url', 'id', 'email', 'name', 'last_name', 'account_address', 'password', )
-        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'password')
+        # fields = ('id', 'email', 'username', 'first_name', 'last_name', 'password')
+        fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'password'
+        )
+
+    # todo
+    def to_representation(self, instance):
+        return UserAfterCreateSerializer(
+            context=self.context).to_representation(instance)
