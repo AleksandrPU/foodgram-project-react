f2bd79d7b2825efb663c6c6b0b2b8c108a575048
Паутов Александр <sh2d000w@gmail.com>
2024-01-24 20:35:12 +0300

    fix: Change urls for users app. fix: Move spectacular urls under DEBUG.

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,15 +1,38 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/users/', include('users.urls')),
-    path('api/', include('users.urls')),
+    path('api/users/', include('users.urls')),
     path('api/auth/', include('users.urls')),
-    # spectacular
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/', include('recipes.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += [
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path(
+            'api/schema/swagger-ui/',
+            SpectacularSwaggerView.as_view(url_name='schema'),
+            name='swagger-ui'
+        ),
+        path(
+            'api/schema/redoc/',
+            SpectacularRedocView.as_view(url_name='schema'),
+            name='redoc'
+        )
+    ]
