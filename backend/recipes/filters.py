c9b994ffe6bd3fbab13e384f139ed23c4c319bc5
Паутов Александр <sh2d000w@gmail.com>
2024-01-24 20:38:45 +0300

    feat: Add FilterSet for ingredient view.

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,8 +1,13 @@
+# todo
+from pprint import pprint
+
+from django.db.models import Case, Q, Value, When
 from django_filters import rest_framework as filters
 
-from recipes.models import Recipe, Tag
+from recipes.models import Ingredient, Recipe, Tag
 
 
+# todo
 class RecipeFilterSet(filters.FilterSet):
     author = filters.NumberFilter(field_name='author')
     tags = filters.ModelMultipleChoiceFilter(
@@ -10,7 +15,41 @@ class RecipeFilterSet(filters.FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
+    is_favorited = filters.BooleanFilter(method='favorited_filter')
+    # 'is_in_shopping_cart',
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags',)
+        # fields = ('author', 'tags', 'is_favorited')
+        fields = ('author', 'tags')
+
+    def favorited_filter(self, queryset, name, value):
+        print('******************')
+        pprint(self.__dict__)
+        print(queryset)
+        print(name)
+        print(value)
+        print('******************')
+        user = self.request.user
+        print(user)
+        if user.is_authenticated:
+            return user.favorites.all()
+        return queryset
+
+
+# todo
+class IngredientFilterSet(filters.FilterSet):
+    name = filters.CharFilter(method='name_filter')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
+
+    def name_filter(self, queryset, name, value):
+        query_istartswith = Q(name__istartswith=value)
+        query_icontains = Q(name__icontains=value)
+        return (queryset.filter(query_istartswith | query_icontains)
+                .annotate(search_type_ordering=Case(
+                    When(query_istartswith, then=Value(1)),
+                    When(query_icontains, then=Value(0)),
+                )).order_by('-search_type_ordering',))
