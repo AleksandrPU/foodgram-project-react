73b88a37ba0fb166221328b6be6717f53d9f8450
Паутов Александр <sh2d000w@gmail.com>
2024-01-28 12:48:49 +0300

    fix: Change permission IsAuthorOrReadOnly.

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return super().has_permission(request, view)
+        return ((request.method in SAFE_METHODS)
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return ((request.method in SAFE_METHODS)
