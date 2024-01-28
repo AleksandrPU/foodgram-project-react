572094178a8f675cac8cf7c43bf11882f7d4d974
Паутов Александр <sh2d000w@gmail.com>
2024-01-28 20:11:11 +0300

    fix: Move pagintions to foodgram_backend.

--- backend/users/paginations.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-from foodgram_backend.constants import PAGINATION_PAGE_SIZE
-
-
-class CustomPageNumberPagination(PageNumberPagination):
-    page_size = PAGINATION_PAGE_SIZE
-    page_size_query_param = 'limit'
