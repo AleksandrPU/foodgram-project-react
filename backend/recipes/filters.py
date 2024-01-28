e60fbaaa88b05aa03aefa47ae65aed87dc12b9ea
Паутов Александр <sh2d000w@gmail.com>
2024-01-28 09:31:39 +0300

    feat: Add filter of favorited and shopping_cart in recipes.

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,6 +1,3 @@
-# todo
-from pprint import pprint
-
 from django.db.models import Case, Q, Value, When
 from django_filters import rest_framework as filters
 
@@ -9,35 +6,44 @@ from recipes.models import Ingredient, Recipe, Tag
 
 # todo
 class RecipeFilterSet(filters.FilterSet):
+    STATUS_CHOICES = (
+        (0, False),
+        (1, True)
+    )
     author = filters.NumberFilter(field_name='author')
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
-    is_favorited = filters.BooleanFilter(method='favorited_filter')
-    # 'is_in_shopping_cart',
+    is_favorited = filters.ChoiceFilter(
+        method='favorited_filter', choices=STATUS_CHOICES)
+    is_in_shopping_cart = filters.ChoiceFilter(
+        method='shopping_filter', choices=STATUS_CHOICES)
 
     class Meta:
         model = Recipe
-        # fields = ('author', 'tags', 'is_favorited')
         fields = ('author', 'tags')
 
     def favorited_filter(self, queryset, name, value):
-        print('******************')
-        pprint(self.__dict__)
-        print(queryset)
-        print(name)
-        print(value)
-        print('******************')
         user = self.request.user
-        print(user)
         if user.is_authenticated:
-            return user.favorites.all()
+            if value == '1':
+                return queryset.filter(favorites__user=user)
+            else:
+                return queryset.filter(~Q(favorites__user=user))
+        return queryset
+
+    def shopping_filter(self, queryset, name, value):
+        user = self.request.user
+        if user.is_authenticated:
+            if value == '1':
+                return queryset.filter(shopping__user=user)
+            else:
+                return queryset.filter(~Q(shopping__user=user))
         return queryset
 
 
-# todo
 class IngredientFilterSet(filters.FilterSet):
     name = filters.CharFilter(method='name_filter')
 
