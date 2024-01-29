df9c9617377f198d2f9d3e37321bb1b554f8d9d6
Паутов Александр <sh2d000w@gmail.com>
2024-01-30 00:31:20 +0300

    fix: Delete unused pip package. fix: Change docs url in README.md. fix: Move .env.example to infra.

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -2,11 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,16 +16,3 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT
     )
-    urlpatterns += [
-        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-        path(
-            'api/schema/swagger-ui/',
-            SpectacularSwaggerView.as_view(url_name='schema'),
-            name='swagger-ui'
-        ),
-        path(
-            'api/schema/redoc/',
-            SpectacularRedocView.as_view(url_name='schema'),
-            name='redoc'
-        )
-    ]
