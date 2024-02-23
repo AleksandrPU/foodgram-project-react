3e0c0c370ef36ad62491a30fa6a6d2d05b2c8713
Паутов Александр <sh2d000w@gmail.com>
2024-02-23 13:08:52 +0300

    fix: Delete unused import.

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,5 +1,4 @@
 import csv
-import logging
 from io import StringIO
 from typing import Optional, Union, Type
 
@@ -10,7 +9,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer, ModelSerializer
+from rest_framework.serializers import Serializer
 
 from foodgram_backend.paginations import CustomPageNumberPagination
 from recipes.filters import IngredientFilterSet, RecipeFilterSet
